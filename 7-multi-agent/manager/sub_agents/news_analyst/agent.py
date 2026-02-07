9fbc4ba7293359bdee93dd4a75008963b2733773
Tony Degollado <antoniodegollado10@gmail.com>
2026-02-07 16:21:07 -0600

    7 Multi-Agent

---
+++ 7-multi-agent/manager/sub_agents/news_analyst/agent.py
@@ -0,0 +1,20 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+news_analyst = Agent(
+    name="news_analyst",
+    model="gemini-2.0-flash",
+    description="News analyst agent",
+    instruction="""
+    You are a helpful assistant that can analyze news articles and provide a summary of the news.
+
+    When asked about news, you should use the google_search tool to search for the news.
+
+    If the user ask for news using a relative time, you should use the get_current_time tool to get the current time to 
+    use in the search query.
+    
+    If the user asks about anything else, 
+    you should delegate the task to the manager agent.
+    """,
+    tools=[google_search]
+)
